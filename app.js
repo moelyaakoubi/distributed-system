580fa6cd8bff309ee5283d599daf0fc69b0c4749
Ayoub <ja_khenfouf@esi.dz>
2024-11-26 14:54:46 +0100

    tp2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";  // Update this if the backend URL is different
+const API_URL = "http://localhost:8000";  // Update this if the backend URL is different
 
 // Fetch all personnel
 async function fetchPersonnel() {
--- models/article.py
+++ models/article.py
@@ -8,3 +8,11 @@ class Article(BaseModel):
     Position: str  # Format "latitude,longitude"
     Rotation: str  # Format "angleX,angleY,angleZ"
     Collisions: int
+
+class Personnel(BaseModel):
+    Identifiant: int  # Numéro de sécurité sociale
+    NomPrenom: str  # Nom-Prénom
+    Etat: str  # "actif", "repos", "arrêt maladie", "congé"
+    Service: str  # "commercial", "finance et gestion", "ressources humaines", "juridique", "logistique", "assistance commerciale", "direction générale", "maintenance", "achats", "cyber sécurité", "recherche et développement", "informatique", "qualité", "collecte", "marketing", "industriel", "assistance technique", "analyse des données"
+    FrequenceCardiaqueActuelle: int  # pulsations/min
+    Position: str  # coordonnées GPS
