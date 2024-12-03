a53d8981cb775b2e5615b6d1f14ee0e06e6cf74b
Ayoub <ja_khenfouf@esi.dz>
2024-12-03 12:30:58 +0100

    tp3

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const API_URL = "http://localhost:8000";  // Update this if the backend URL is different
+const API_URL = "http://localhost:8000";
+const API_URL1 = "https://56ba-176-183-113-247.ngrok-free.app";
+const API_URL4 = "https://d98f-194-199-84-88.ngrok-free.app"
+
 
 // Fetch all personnel
 async function fetchPersonnel() {
     try {
-        const response = await fetch(`${API_URL}/personnel`);
+        const response = await fetch(`${API_URL}/personnel`,
+            {
+                method: "get",
+                headers: new Headers({
+                  "ngrok-skip-browser-warning": "true",
+                }),
+            }
+        );
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -18,7 +28,14 @@ async function fetchPersonnel() {
 
 // Fetch all sanitary readings
 function fetchSanitaryReadings() {
-    fetch(`${API_URL}/relevessanitaires`)
+    fetch(`${API_URL}/relevessanitaires`,
+        {
+            method: "get",
+            headers: new Headers({
+              "ngrok-skip-browser-warning": "true",
+            }),
+        }
+    )
         .then(response => response.json())
         .then(data => displayResults(data, "Sanitary Readings"))
         .catch(error => displayError(error));
@@ -26,7 +43,14 @@ function fetchSanitaryReadings() {
 
 // Fetch all articles
 function fetchArticles() {
-    fetch(`${API_URL}/articleschainemontage`)
+    fetch(`${API_URL}/articleschainemontage`,
+        {
+            method: "get",
+            headers: new Headers({
+              "ngrok-skip-browser-warning": "true",
+            }),
+        }
+    )
         .then(response => response.json())
         .then(data => displayResults(data, "Articles List"))
         .catch(error => displayError(error));
@@ -35,8 +59,8 @@ function fetchArticles() {
 // Create a new article
 function createArticle() {
     const article = {
-        IdentifiantProduit: 101,
-        Zone: 2,
+        IdentifiantProduit: 105,
+        Zone: 1548,
         EtatEmballage: "Correct",
         Responsable: 123456789012345,
         Position: "29.9789,31.1342",
@@ -48,6 +72,7 @@ function createArticle() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            "ngrok-skip-browser-warning": "true"
         },
         body: JSON.stringify(article)
     })
@@ -66,4 +91,94 @@ function displayResults(data, title) {
 function displayError(error) {
     const resultArea = document.getElementById("response");
     resultArea.innerHTML = `<h3>Error</h3><pre>${error}</pre>`;
+}
+
+
+async function fetchHighestMotivationFormation() {
+    try {
+        const [response1, response2] = await Promise.all([
+            fetch(`${API_URL}/forma_max_motivation`, {
+                method: "get",
+                headers: new Headers({
+                  "ngrok-skip-browser-warning": "true",
+                }),
+            }),
+            fetch(`${API_URL4}/forma_max_motivation`, {
+                method: "get",
+                headers: new Headers({
+                  "ngrok-skip-browser-warning": "true",
+                }),
+            })
+        ]);
+
+        if (!response1.ok || !response2.ok) {
+            throw new Error(`HTTP error! Status: ${response1.status} ${response2.status}`);
+        }
+
+        const [data1, data2] = await Promise.all([response1.json(), response2.json()]);
+
+        const allFormations = [...data1,...data2]
+
+        console.log(allFormations);
+
+        const highestMotivationFormation = allFormations.reduce((prev, current) => {
+            return (prev.PourcentageEngagement > current.PourcentageEngagement) ? prev : current;
+        });
+
+        displayCardResult(highestMotivationFormation);
+    } catch (error) {
+        displayError(error.message);
+    }
+}
+
+function displayCardResult(data) {
+    const cardElement = document.querySelector(".card-rh .value");
+    cardElement.innerHTML = `${data.PourcentageEngagement}%`;
+    const descriptionElement = document.querySelector(".card-rh .description");
+    descriptionElement.innerHTML = `${data.NomFormation}`;
+}
+
+
+async function articleAyantMinCollision() {
+    try {
+        const [response1, response2] = await Promise.all([
+            fetch(`${API_URL}/Commercial/A`, {
+                method: "get",
+                headers: new Headers({
+                  "ngrok-skip-browser-warning": "true",
+                }),
+            }),
+            fetch(`${API_URL1}/Commercial/A`, {
+                method: "get",
+                headers: new Headers({
+                  "ngrok-skip-browser-warning": "true",
+                }),
+            })
+        ]);
+
+        if (!response1.ok || !response2.ok) {
+            throw new Error(`HTTP error! Status: ${response1.status} ${response2.status}`);
+        }
+
+        const [data1, data2] = await Promise.all([response1.json(), response2.json()]);
+
+        const articles = [...data1,...data2]
+
+        console.log(articles);
+
+        const articleAyantMinCollision = articles.reduce((prev, current) => {
+            return (prev.Collisions > current.Collisions) ? prev : current;
+        });
+
+        displayArticleAyantMinCollisionCardResult(articleAyantMinCollision);
+    } catch (error) {
+        displayError(error.message);
+    }
+}
+
+function displayArticleAyantMinCollisionCardResult(data) {
+    const cardElement = document.querySelector(".card-commercial .value");
+    cardElement.innerHTML = `${data.IdentifiantProduit}`;
+    const descriptionElement = document.querySelector(".card-commercial .description");
+    descriptionElement.innerHTML = `${data.Collisions}`;
 }
--- main.py
+++ main.py
@@ -45,11 +45,123 @@ async def all():
     connection.close()
     return rows
 
+@app.get("/count_personnel_rh")
+async def count_personnel_rh():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT COUNT(*) as count FROM personnel WHERE service="ressources humaines"')
+    result = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return {"count": result['count']}
+
+@app.get("/formations")
+async def all():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM formations')
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+@app.get("/formations_rh")
+async def formations_rh():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM formations WHERE sujet="ressources humaines"')
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+@app.get("/forma_max_motivation")
+async def get_max_motivation_formation():
+    """
+    Récupère la formation en ressources humaines avec le plus haut pourcentage d'engagement.
+    """
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        
+        query = (
+            """
+            SELECT NomFormation, PourcentageEngagement
+            FROM formations 
+            WHERE Sujet = %s AND PourcentageEngagement = (
+                SELECT MAX(PourcentageEngagement) 
+                FROM formations 
+                WHERE Sujet = %s
+            )
+            """
+        )
+        sujet = "ressources humaines"
+        cursor.execute(query, (sujet, sujet))
+        rows = cursor.fetchall()
+        
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+    return rows
+
+@app.get("/nb_articles_emballage_correct")
+async def get_articles_with_correct_packaging():
+    """
+    Récupère le nombre d'articles avec un emballage correct.
+    """
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        
+        query = """
+            SELECT COUNT(*) AS NombreArticles 
+            FROM articleschainemontage 
+            WHERE EtatEmballage = %s
+        """
+        etat = "Correct"
+        cursor.execute(query, (etat,))
+        rows = cursor.fetchall()
+        
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+    return rows
+
+@app.get("/has_personnel_rh")
+async def has_personnel_rh():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT COUNT(*) as count FROM personnel WHERE service="ressources humaines"')
+    result = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return {"has_personnel_rh": result['count'] > 0}
+
 @app.get("/relevessanitaires")
 async def all():
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
     cursor.execute('SELECT * FROM relevessanitaires')
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+@app.get("/relevessanitaires/max_temp")
+async def all():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT Zone, max(TemperatureAir) as max_temp FROM relevessanitaires')
     rows = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -93,5 +205,15 @@ async def create_article(article: Article):
     connection.close()
     
     return {**article.dict()}  # Assuming `Article` has a `dict` method for serialization
+
+@app.get("/Commercial/A")
+async def less_collisions():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM articleschainemontage ORDER BY Collisions ASC LIMIT 1')
+    article = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return article
 
 
